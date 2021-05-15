c8751376b58375df4fe9a257c34d9a128574751d
Drew Stone <drewstone329@gmail.com>
2021-05-15 16:20:10 +0300

    Remove old distribution

--- quaddrop/src/lib.rs
+++ quaddrop/src/lib.rs
@@ -1,4 +1,3 @@
-use hex::FromHex;
 use serde_json::Result;
 use std::fs::File;
 use std::io::Read;
@@ -6,23 +5,21 @@ use std::path::Path;
 use serde::{Serialize, Deserialize};
 use rococo_parachain_primitives::*;
 
-pub mod mainnet_fixtures;
+pub mod distribution;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Allocation {
 	pub balances: Vec<(AccountId, Balance)>,
-	pub vesting: Vec<(AccountId, BlockNumber, BlockNumber, Balance)>,
 }
 
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AllocationRaw {
 	balances: Vec<(String, String)>,
-	vesting: Vec<(String, BlockNumber, BlockNumber, String)>,
 }
 
-pub fn get_lockdrop_participants_allocation() -> Result<AllocationRaw> {
-	let path = Path::new("rococo-parachains/lockdrop/allocation/dump.json");
+pub fn get_quaddrop_allocation() -> Result<AllocationRaw> {
+	let path = Path::new("quaddrop/allocation/dump.json");
 	let mut file = File::open(&path).unwrap();
 	let mut data = String::new();
 	file.read_to_string(&mut data).unwrap();
@@ -31,44 +28,9 @@ pub fn get_lockdrop_participants_allocation() -> Result<AllocationRaw> {
 }
 
 pub fn parse_allocation() -> Result<Allocation> {
-	let allocation = get_lockdrop_participants_allocation().unwrap();
-	let mut balances: Vec<(AccountId, Balance)> = allocation
-		.balances
-		.iter()
-		.map(|b| {
-			let balance = b.1.to_string().parse::<Balance>().unwrap();
-			return (<[u8; 32]>::from_hex(b.0.clone()).unwrap().into(), balance);
-		})
-		.filter(|b| b.1 > 0)
-		.collect();
-
-	let mainnet_alloc = mainnet_fixtures::get_commonwealth_allocation();
-	for i in 0..balances.len() {
-		for j in 0..mainnet_alloc.len() {
-			if balances[i].0 == mainnet_alloc[j].0 {
-				balances[i].1 += mainnet_alloc[j].1
-			}
-		}
-	}
-
-	let vesting = allocation
-		.vesting
-		.iter()
-		.map(|b| {
-			let vesting_balance = b.3.to_string().parse::<Balance>().unwrap();
-			return (
-				(<[u8; 32]>::from_hex(b.0.clone()).unwrap()).into(),
-				b.1,
-				b.2,
-				vesting_balance,
-			);
-		})
-		.filter(|b| b.3 > 0)
-		.collect();
-
+	let balances = distribution::get_allocation();
 	Ok(Allocation {
 		balances,
-		vesting,
 	})
 }
 
