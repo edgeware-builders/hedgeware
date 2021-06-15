8ddf8bd7946bd71da1ca53a8bb16448818ca4ae7
Drew Stone <drewstone329@gmail.com>
2021-06-15 14:29:48 -0400

    Ups

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -966,6 +966,8 @@ impl merkle::Config for Runtime {
 	type Event = Event;
 	type MaxTreeDepth = MaxTreeDepth;
 	type TreeId = u32;
+	type KeyId = u32;
+	type Randomness = RandomnessCollectiveFlip;
 	type WeightInfo = MerkleWeights<Self>;
 }
 
